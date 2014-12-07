413e9c7288e9638d1eb66ead0571ca7d4a117024
nmartin <nmartin867@gmail.com>
2014-12-07 15:46:02 -0700

    init

---
+++ app.js
@@ -0,0 +1,19 @@
+var express = require('express');
+var app = express();
+
+
+app.set('view engine', 'ejs');
+app.set('views', __dirname + '/public/views');
+app.use(express.static(__dirname + '/public'));
+
+var home = require('./routes/home');
+var image = require('./routes/image');
+
+app.use('/',home);
+app.use('/image', image);
+
+var server = app.listen(3000, function () {
+    var host = server.address().address;
+    var port = server.address().port;
+    console.log('Example app listening at http://%s:%s', host, port)
+});
---
+++ routes/image.js
@@ -0,0 +1,34 @@
+var express = require('express');
+var router = express.Router();
+var exec = require('child_process').exec;
+
+
+router.get('/', function (req, res) {
+    var options = {
+        root: __dirname + '/public/capture'
+    };
+    res.sendFile('image.jpg', options, function (err) {
+        if (err) {
+            res.status(err.status).end();
+        }
+    });
+});
+
+// POST /image/:width/:height
+router.post('/:width/:height', function (req, res) {
+    var cmd = 'raspistill -w ' + req.params.width + ' -h ' + req.params.height + ' -o ./public/captures/image.jpg --timeout 1 --nopreview';
+    exec(cmd, function (err, stdout, stderr) {
+        if (err) {
+            res.status(500);
+            res.json({cmd: cmd, errmessage: err.message});
+        } else if (stderr) {
+            res.status(500);
+            res.json({cmd: cmd, errmessage: stdout});
+        } else {
+            res.status(200);
+            res.end();
+        }
+    });
+});
+
+module.exports = router;
